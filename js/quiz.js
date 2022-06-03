03bb379c2662f0c2985315c1d0843194fae70677
CarloHFR <carlohenrique78@gmail.com>
2022-06-03 10:49:29 -0300

    fix responsive layout

--- js/quiz.js
+++ js/quiz.js
@@ -95,13 +95,11 @@ var write_question_screen = function()
 {
     if(current_question.image_url)
     {
-        modal = "<div class='modal-header'><div class='text-center quiz-question'><h3 class='modal-title'>" + current_question.question + "</h3><div class='question-img'><img class='img-fluid' src=" + current_question.image_url + "></div></div></div>";
+        modal = "<div class='modal-header'><div class='text-justify quiz-question'><h3 class='modal-title'>" + current_question.question + "</h3><div class='question-img'><img class='img-fluid img-size' src=" + current_question.image_url + "></div></div></div>";
     }else
     {
-        modal = "<div class='modal-header'><div class='text-center quiz-question'><h3 class='modal-title'>" + current_question.question + "</h3><div class='question-img'></div></div></div>";
+        modal = "<div class='modal-header'><div class='text-justify quiz-question'><h3 class='modal-title'>" + current_question.question + "</h3><div class='question-img'></div></div></div>";
     }
-    
-    console.log(modal);
 
     modal += "<div class='modal-body'><div class='quiz' id='quiz' data-toggle='buttons'>";
 
@@ -121,34 +119,7 @@ var write_question_screen = function()
 
     $(".content-modal").html("");
     $(".content-modal").html(modal);
-}; 
-
-
-/*
-var write_question_screen = function()
-{
-    modal = "<div class='modal-header'><h3 class='col-12 modal-title text-center'><span class='label label-warning' id='qid'></span>" + current_question.question + "</h3></div>";
-    modal += "<div class='modal-body'><div class='quiz' id='quiz' data-toggle='buttons'>";
-
-    if(current_question.type == "multi")
-    {
-        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
-        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
-        modal += "<button class='element-animation3 btn btn-lg btn-light btn-block text-info' value='a3' onclick='check_answer(this)'>" + current_question.answers.a3 + "</button>";
-        modal += "<button class='element-animation4 btn btn-lg btn-light btn-block text-info' value='a4' onclick='check_answer(this)'>" + current_question.answers.a4 + "</button>";
-    }else if(current_question.type == "vf")
-    {
-        modal += "<button class='element-animation1 btn btn-lg btn-light btn-block text-info' value='a1' onclick='check_answer(this)'>" + current_question.answers.a1 + "</button>";
-        modal += "<button class='element-animation2 btn btn-lg btn-light btn-block text-info' value='a2' onclick='check_answer(this)'>" + current_question.answers.a2 + "</button>";
-    }
-    
-    modal += "</div></div><div class='modal-footer' style='justify-content: space-between;'><div class='quiz-bold-white-content link-footer' onclick='restart_quiz()'>Recomeçar</div><span class='quiz-bold-white-content'>Pontuação: " + score + "</span></div>";
-
-    $(".content-modal").html("");
-    $(".content-modal").html(modal);
-}; 
-
-*/
+};
 
 
 var write_final_screen = function()
